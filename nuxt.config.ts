480382c23ec0f5c9fee094331b1881a7d6186da3
Pavel Kuzmin <Virus191288@gmail.com>
2024-11-04 23:58:18 +0500

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // https://nuxt.com/modules
+  modules: [
+    '@nuxthub/core',
+    'nuxt-i18n-micro',
+    '@nuxt/eslint',
+  ],
+
+  // https://devtools.nuxt.com
+  devtools: { enabled: true },
+
+  // Env variables - https://nuxt.com/docs/getting-started/configuration#environment-variables-and-private-tokens
+  runtimeConfig: {
+    public: {
+      // Can be overridden by NUXT_PUBLIC_HELLO_TEXT environment variable
+      helloText: 'Hello from the Edge 👋',
+    },
+  },
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-07-30',
+
+  // https://hub.nuxt.com/docs/getting-started/installation#options
+  hub: {},
+
+  i18n: {
+    locales: [
+      { code: 'en', iso: 'en_EN', displayName: 'English' },
+      { code: 'de', iso: 'de_DE', displayName: 'German' },
+    ],
+    defaultLocale: 'en',
+    includeDefaultLocaleRoute: true,
+  },
+
+  // https://eslint.nuxt.com
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: 'single',
+      },
+    },
+  },
+})
